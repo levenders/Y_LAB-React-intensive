151cb94818d0767d66ba6bfc27649c9c664bdb6f
Lev Enders <leveders@mail.ru>
2024-09-04 23:13:40 +0300

    feat: add mockFetch api to request

---
+++ src/api/Api.ts
@@ -0,0 +1,31 @@
+interface RequestProps {
+  url: string
+  method?: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  body?: any
+}
+
+interface ResponseProps {
+  ok: boolean
+  json: () => Promise<any>
+}
+
+const mockFetch = async (): Promise<ResponseProps> => {
+  return {
+    ok: true,
+    json: async () => ({ message: 'Вы успешно авторизованы' }),
+  }
+}
+
+export async function ApiClient({ url, method = 'GET', body }: RequestProps) {
+  const params = { url, method, body }
+
+  const response = await mockFetch()
+
+  const data = await response.json()
+
+  if (!response.ok) {
+    throw new Error(data ?? 'Неизвестная ошибка')
+  }
+
+  console.log(data, params.body)
+}
