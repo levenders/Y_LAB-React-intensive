b694980d71d8d4296e3365725fea855c79933e6f
Lev Enders <leveders@mail.ru>
2024-09-05 21:49:24 +0300

    feat: add validate into form

--- src/components/Form/Form.style.ts
+++ src/components/Form/Form.style.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
+  position: relative;
   gap: 30px;
   min-width: 300px;
 `
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -9,8 +9,9 @@ import {
   useRef,
   useState,
 } from 'react'
+import { flushSync } from 'react-dom'
 
-import { Button, Input } from '@/components'
+import { Button, Input, Validation } from '@/components'
 
 import { StyledForm } from './Form.style'
 
@@ -23,6 +24,8 @@ export const Form = ({ title, handleClick }: FormProps) => {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
 
+  const [error, setError] = useState<string | null>(null)
+
   const inputRef = useRef<HTMLInputElement>(null)
 
   const focusInput = () => {
@@ -33,6 +36,17 @@ export const Form = ({ title, handleClick }: FormProps) => {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
+    flushSync(() => {
+      // необходимо для принудительного синхронного обновления, иначе Fiber не дает сбросить состояние
+      setError(null)
+    })
+
+    if (!email.trim() || !password.trim()) {
+      setError('Необходимо заполнить все поля!')
+      return
+    }
+
     handleClick({ email, password })
     setEmail('')
     setPassword('')
@@ -51,6 +65,7 @@ export const Form = ({ title, handleClick }: FormProps) => {
 
   return (
     <StyledForm onSubmit={handleSubmit}>
+      {error && <Validation>{error}</Validation>}
       <Input
         type="email"
         value={email}
